ea153f820471773f2d68d4e193e188d3b16b77c8
Your Name <you@example.com>
2023-02-13 14:55:34 +0100

    about page meta

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,3 +1,12 @@
 self.__RSC_CSS_MANIFEST={
-  "__entry_css_mods__": {}
+  "__entry_css_mods__": {
+    "/home/user/Documents/DCI_HTML/nextjs-usecontext/app/page": [
+      "/home/user/Documents/DCI_HTML/nextjs-usecontext/styles/site.scss",
+      "/home/user/Documents/DCI_HTML/nextjs-usecontext/styles/globals.css"
+    ]
+  },
+  "/home/user/Documents/DCI_HTML/nextjs-usecontext/app/layout.tsx": [
+    "/home/user/Documents/DCI_HTML/nextjs-usecontext/styles/site.scss",
+    "/home/user/Documents/DCI_HTML/nextjs-usecontext/styles/globals.css"
+  ]
 }
